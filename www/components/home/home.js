87838ef0b1ac2d3ac4304c1c373f477ee5c3b544
Gus Nordhielm <gnordhielm@gmail.com>
2017-07-08 17:05:34 -0700

    make home data kinda dynamic

--- www/components/home/home.js
+++ www/components/home/home.js
@@ -9,5 +9,7 @@ angular
 
 function controller() {
   var vm = this
-  vm.name = 'home'
+  vm.title = 'Almost Famous'
+  vm.info = 'Starring Kate Hudson'
+  vm.imageUrl = 'https://d19p213wjrwt85.cloudfront.net/uvvu-images/0941ADA74230B31EE05316345B0AA37F'
 }
